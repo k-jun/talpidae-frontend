1f13234300927de17ba25ee6f44154e426e3a8e8
keijun-kumagai <keijun.kumagai@dena.com>
2021-01-11 13:57:01 +0900

    feat(all): change field, height, and width

--- src/pages/Game.svelte
+++ src/pages/Game.svelte
@@ -3,7 +3,7 @@
 	import FieldState from "../logics/field";
 	import Controller from "../logics/controller";
 
-	let field = new FieldState({ height: 250, width: 150 });
+	let field = new FieldState({ height: 150, width: 80 });
 	let current_width = Math.floor(Math.random() * field.width);
 	let current_height = Math.floor(Math.random() * field.height);
 	let controller = new Controller({
